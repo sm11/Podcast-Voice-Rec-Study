583250688c06c58b4a9494121e30e1b5459a1046
sm11 <31568178+sm11@users.noreply.github.com>
2017-12-21 21:37:00 -0500

    Delete tts2.sh

--- helpers/tts2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-parentdir="$(dirname "$(pwd)")"
-echo $parentdir
-aws polly synthesize-speech \
---text-type ssml \
---text file://$parentdir/static/testfile.xml \
---output-format mp3 \
---voice-id Joanna \
-$parentdir/static/speech.mp3
